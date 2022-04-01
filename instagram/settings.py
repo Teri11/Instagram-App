7c50526e38ec26a6ac219fe0a79105b7b36a24ad
Teri11 <augustineteri157@gmail.com>
2022-04-01 16:54:09 +0300

    added navbar

--- instagram/settings.py
+++ instagram/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos',
+    'photos.apps.PhotosConfig'
 ]
 
 MIDDLEWARE = [
--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to my instagram!")
+    templates =loader.get_template('index.html')
+    return HttpResponse(templates.render())
